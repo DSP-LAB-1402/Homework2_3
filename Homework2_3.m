ef986f2b962b84e560528d5bb062d6a798a1f6b3
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-11-12 17:04:55 +0330

    Comment details of Homework2_3.m

--- Homework2_3.m
+++ Homework2_3.m
@@ -8,13 +8,21 @@ Close all;
 Clear;
 clc;
 %% Homework_1
+%%%
+% In this Homework we play with wavelet functions and how it effects on
+% signal
+%
+%%%
+% Here we declare some basic variables to solve the first question
 
 N = 10;
 loc = linspace(0, 1, 2 ^ N);
 loc_f = linspace(-500, 500, 2 ^ N);
 sqrtsnr = 10;
+%%%
+% Here we want to add white noise to our doppler signal and plot them
 [x, noisy_x] = wnoise('doppler', N, sqrtsnr);
-figure('Name', 'doppler Signal vs Heavy Sine Signal with White Noise');
+figure('Name', 'Doppler Signal vs Doppler Signal with White Noise');
 subplot(211);
 plot(loc, x, 'LineWidth', 1.5);
 title('Doppler Signal');
@@ -28,7 +36,11 @@ xlabel('Time');
 ylabel('Amplitude');
 grid on;
 
-figure('Name', 'doppler Signal vs Heavy Sine Signal with White Noise FFT');
+%%%
+% Part 2:
+%
+% Now we plot FFT of noisy signal and original signal
+figure('Name', 'Doppler Signal vs Doppler Signal with White Noise FFT');
 subplot(211);
 plot(loc_f, abs(fftshift(fft(x))), 'LineWidth', 1.5);
 title('Doppler Signal FFT');
@@ -41,6 +53,11 @@ title('Doppler Signal with White Noise FFT');
 xlabel('Frequency(Hz)');
 ylabel('Amplitude');
 grid on;
+
+%%%
+% Part3:
+%
+% Here we want to extract Coefficients of our noisy signal and then plot them
 
 [cA, cD] = dwt(noisy_x, 'db10');
 figure('Name', 'Approximation and Detail Coefficients');
@@ -58,7 +75,10 @@ xlabel('Time');
 ylabel('Amplitude');
 grid on;
 axis tight;
-
+%%%
+% Part 4:
+%
+% Here we want to extract to 10th level of coefficients and plot them
 numberOfLevels = 10;
 [waveletCoefficients, waveletLevels] = wavedec(noisy_x, 10, 'db10');
 figure('Name', 'Wavelet Coefficients');
@@ -67,7 +87,11 @@ title('Wavelet Coefficients');
 xlabel('Time');
 ylabel('Amplitude');
 grid on;
-
+%%%
+% Part 5:
+%
+% Here we want to extract detailed coefficient of wavelet coefficient
+% and plot them.
 detailed_coeff = detcoef(waveletCoefficients, waveletLevels, 1:numberOfLevels);
 figure('Name', 'Wavelet Coefficients of Different Levels');
 set(gcf, 'Position', [100, 100, 1000, 1000]);
@@ -80,6 +104,69 @@ for i = 1:numberOfLevels
     ylabel('Amplitude');
     grid on;
 end
+
+%%%
+% Part 6:
+%
+% Here we want to extract the deepest approximated layer
+% and then we want to reconstruct signal from approximated
+% and detailed coefficient and then see how much is the error
+% and at last we want to plot them
+approx_coeff_deepest_layer = appcoef( ...
+    waveletCoefficients, ...
+    waveletLevels, ...
+    'db10', ...
+    numberOfLevels ...
+);
+wavelet_coefficients_regenerated = ...
+    [approx_coeff_deepest_layer, detailed_coeff{numberOfLevels:-1:1}];
+%%%
+% Part 7:
+%
+% Here we want to extract approximated coefficient from wavelet coefficient
+
+fprintf("\nError: %d\n", ...
+    sum(abs(wavelet_coefficients_regenerated - waveletCoefficients)));
+figure('Name', 'Wavelet Coefficients vs Regenerated Wavelet Coefficients');
+subplot(211);
+stem(waveletCoefficients, 'LineWidth', 1.5);
+title('Wavelet Coefficients');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+axis tight;
+subplot(212);
+stem(wavelet_coefficients_regenerated, 'LineWidth', 1.5);
+title('Regenerated Wavelet Coefficients');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+axis tight;
+
+% DetCoef = detcoef(waveletCoefficients, waveletLevels, [1 2 3]);
+% figure('Name', 'Wavelet Coefficients of Level 1 and 2');
+% subplot(311);
+% stem(DetCoef{1}, 'LineWidth', 1.5);
+% title('Wavelet Coefficients of Level 1 ');
+% xlabel('Time');
+% ylabel('Amplitude');
+% grid on;
+% subplot(312);
+% stem(DetCoef{2}, 'LineWidth', 1.5);
+% title('Wavelet Coefficients of Level 2');
+% xlabel('Time');
+% ylabel('Amplitude');
+% grid on;
+% subplot(313);
+% stem(DetCoef{3}, 'LineWidth', 1.5);
+% title('Wavelet Coefficients of Level 3 ');
+% xlabel('Time');
+% ylabel('Amplitude');
+% grid on;
+%%%
+% Part 8:
+%
+% We want to calculate approximated coefficient of each level
 
 approx_coeff = appcoef(waveletCoefficients, waveletLevels, 'db10', 10);
 figure('Name', 'Approximation Coefficients');
@@ -102,55 +189,14 @@ for i = 1:numberOfLevels
     grid on;
 end
 
-approx_coeff_deepest_layer = appcoef( ...
-    waveletCoefficients, ...
-    waveletLevels, ...
-    'db10', ...
-    numberOfLevels ...
-);
-wavelet_coefficients_regenerated = ...
-    [approx_coeff_deepest_layer, detailed_coeff{numberOfLevels:-1:1}];
-
-fprintf("\nError: %d\n", ...
-    sum(abs(wavelet_coefficients_regenerated - waveletCoefficients)));
-figure('Name', 'Wavelet Coefficients vs Regenerated Wavelet Coefficients');
-subplot(211);
-stem(waveletCoefficients, 'LineWidth', 1.5);
-title('Wavelet Coefficients');
-xlabel('Time');
-ylabel('Amplitude');
-grid on;
-axis tight;
-subplot(212);
-stem(wavelet_coefficients_regenerated, 'LineWidth', 1.5);
-title('Regenerated Wavelet Coefficients');
-xlabel('Time');
-ylabel('Amplitude');
-grid on;
-axis tight;
-
-DetCoef = detcoef(waveletCoefficients, waveletLevels, [1 2 3]);
-figure('Name', 'Wavelet Coefficients of Level 1 and 2');
-subplot(311);
-stem(DetCoef{1}, 'LineWidth', 1.5);
-title('Wavelet Coefficients of Level 1 ');
-xlabel('Time');
-ylabel('Amplitude');
-grid on;
-subplot(312);
-stem(DetCoef{2}, 'LineWidth', 1.5);
-title('Wavelet Coefficients of Level 2');
-xlabel('Time');
-ylabel('Amplitude');
-grid on;
-subplot(313);
-stem(DetCoef{3}, 'LineWidth', 1.5);
-title('Wavelet Coefficients of Level 3 ');
-xlabel('Time');
-ylabel('Amplitude');
-grid on;
-
-numberOfLevels1=3;
+%%%
+% Part 9:
+%
+% Here we want to reconstruct signal with substitute first 3
+% layer of detailed coefficient with 0
+%
+% and then plot it with noisy signal
+numberOfLevels1 = 3;
 approx_coeff_deepest = appcoef( ...
     waveletCoefficients, ...
     waveletLevels, ...
@@ -161,9 +207,9 @@ wavelet_coefficients_regenerated = ...
     [approx_coeff_deepest, detailed_coeff{numberOfLevels1:-1:1}];
 
 x_rec = waverec(waveletCoefficients, waveletLevels, 'db10');
-length1 = idwt(approx_coeff_deepest,zeros(1,144), 'db10');
-length2 = idwt(length1,zeros(1,270), 'db10');
-length3 = idwt(length2,zeros(1,522), 'db10');
+length1 = idwt(approx_coeff_deepest, zeros(1, 144), 'db10');
+length2 = idwt(length1, zeros(1, 270), 'db10');
+length3 = idwt(length2, zeros(1, 522), 'db10');
 
 loc_re = linspace(0, 1, 2 ^ N + 2);
 
@@ -176,7 +222,10 @@ grid on;
 hold on;
 plot(loc_re, length3, 'LineWidth', 1.5);
 legend('Original Signal', 'Reconstructed Signal');
-
+%%%
+% Part 10:
+%
+% here we want to denoise the signal and compare it with original and noisy signal
 denoised_x = wdenoise(noisy_x, 10);
 
 figure('Name', 'Doppler Signal with White Noise vs Denoised Heavy Sine Signal');
@@ -195,8 +244,7 @@ hold on;
 title('Doppler Signal');
 xlabel('Time');
 ylabel('Amplitude');
-plot(loc,x, 'LineWidth', 1.5);
+plot(loc, x, 'LineWidth', 1.5);
 grid on;
 hold on;
-legend('Noisy Signal', 'Denoised Signal','Original Signal');
-
+legend('Noisy Signal', 'Denoised Signal', 'Original Signal');
